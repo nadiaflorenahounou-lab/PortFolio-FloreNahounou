a9dea4df76860096a19bfa0c2190427fac169080
nadiaflorenahounou-lab <nadiaflorenahounou@gmail.com>
2026-02-08 20:15:50 +0000

    MAJ: Noms exacts Dev Journey et Delaroche Fashion avec liens opérationnels

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("Portfolio Nadia Flore NAHOUNOU v40.0 - Opérationnel.");
+    console.log("Portfolio Nadia Flore NAHOUNOU v42.0 - Liens Dev Journey & Delaroche Fashion synchronisés.");
 });
