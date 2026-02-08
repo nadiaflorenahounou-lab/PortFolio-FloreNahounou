f4000dc34a00046e97e392091933ba43e0f70bcc
nadiaflorenahounou-lab <nadiaflorenahounou@gmail.com>
2026-02-08 19:39:44 +0000

    Version finale : Ordre projets et parcours Ministère

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Gestion des boutons de demande d'accès (CMU Smart & Hub Flow)
     const secureButtons = document.querySelectorAll('.btn-request');
-    
     secureButtons.forEach(btn => {
         btn.addEventListener('click', (e) => {
             e.preventDefault();
-            alert("SÉCURITÉ DGCMU : Accès restreint. Votre demande d'autorisation a été transmise à Nadia Flore NAHOUNOU.");
+            alert("SÉCURITÉ DGCMU : Accès restreint. Votre demande a été transmise à Nadia Flore NAHOUNOU.");
         });
     });
-
-    console.log("Portfolio Nadia Flore NAHOUNOU - Version 2026 prête.");
 });
