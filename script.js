6a4dec75c2e393727c57730457c5b7a59ce6f2cc
nadiaflorenahounou-lab <nadiaflorenahounou@gmail.com>
2026-02-08 09:34:36 +0000

    Version finale 100% : Bio officielle, Mentions orange et codes complets

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const secureButtons = document.querySelectorAll('.btn-request');
     secureButtons.forEach(btn => {
         btn.addEventListener('click', (e) => {
-            e.preventDefault();
+            e.preventDefault(); // Empêche le lien de faire sa fonction par défaut
             alert("SÉCURITÉ DGCMU : Cet élément est privé ou confidentiel. Votre demande d'accès a été transmise à Nadia Flore NAHOUNOU.");
         });
     });
