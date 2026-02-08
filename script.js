9cdfd369196bf6e7a7227b97ca047a7a15e1da23
nadiaflorenahounou-lab <nadiaflorenahounou@gmail.com>
2026-02-08 20:08:58 +0000

    Version finale officielle : Portfolio complet 6 fichiers

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const secureButtons = document.querySelectorAll('.btn-request');
-    secureButtons.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            e.preventDefault();
-            alert("SÉCURITÉ DGCMU : Accès restreint. Votre demande a été transmise à Nadia Flore NAHOUNOU.");
-        });
-    });
+    console.log("Portfolio Nadia Flore NAHOUNOU v40.0 - Opérationnel.");
 });
